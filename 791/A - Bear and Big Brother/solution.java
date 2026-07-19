8f519bdac0521088f1e9d475f71ece84ec83c3d0
Sahil Kumar Soni <sahilsoni.technology@gmail.com>
2026-07-19 21:31:40 +0530

    Add Bear and Big Brother [A] from Codeforces

--- 791/A - Bear and Big Brother/solution.java
+++ 791/A - Bear and Big Brother/solution.java
@@ -5,14 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int limak = sc.nextInt();
         int bob = sc.nextInt();
-        int year = 0;
         
-        while (limak <= bob) {
-            limak = limak * 3;
-            bob = bob * 2;
-            year++;
+        // Loop runs indefinitely until the condition inside breaks it
+        for (int years = 1; ; years++) {
+            limak *= 3; // Limak's weight triples
+            bob *= 2;   // Bob's weight doubles
+            
+            if (limak > bob) { 
+                System.out.println(years);
+                break;
+            }
         }
-        
-        System.out.println(year);
+        sc.close();
     }
 }
